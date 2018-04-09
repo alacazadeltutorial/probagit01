9729dc8ae8174bf6918cc5f8d12d13326cfc8a72
alacazadeltutorial <alacazadeltutorial@gmail.com>
2018-04-09 12:16:28 +0200

    Cambios cores

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,6 +1,10 @@
 public class OlaMundo {
 	public static void main (String[] args) {
-		System.out.println("Olá mundo querido");
-		System.out.println("Segunda liña: saúdo ampliado");
+		
+		String laranxa = "\033[33m";
+		String azul = "\033[34m";
+
+		System.out.println(laranxa + "Olá mundo querido");
+		System.out.println(azul + "Segunda liña: saúdo ampliado");
 	}
 }
